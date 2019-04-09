4a6f5594a8d2843aeed3284138db9129e606f1f0
Ronald Flint <ronaldeflint@gmail.com>
2019-04-09 19:22:47 -0400

    Changed reference

--- LoadEmbeddedAssembly/LoadEmbeddedAssembly/Program.cs
+++ LoadEmbeddedAssembly/LoadEmbeddedAssembly/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace LoadEmbeddedAssembly
 {
@@ -6,7 +7,17 @@ namespace LoadEmbeddedAssembly
 	{
 		static void Main(string[] args)
 		{
+			AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+		}
 
+		private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+		{
+			throw new NotImplementedException();
+		}
+
+		private Assembly LoadEmbeddedAssembly(string resourcePath)
+		{
+			var path = Assembly.GetManifestResourceNames()
 		}
 	}
 }
